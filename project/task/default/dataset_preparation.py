ea3554c09ef61e3819417c06c197631bdd1f8d15
Sergio Rivera <sriveralopez50@gmail.com>
2024-03-19 18:43:38 +0000

    Created README.md

--- project/task/default/dataset_preparation.py
+++ project/task/default/dataset_preparation.py
@@ -3,7 +3,7 @@
 Please add here all the necessary logic to either download, uncompress, pre/post-process
 your dataset (or all of the above). If the desired way of running your code is to first
 download the dataset and partition it and then run the experiments, please uncomment the
-lines below and tell us in the README.md (see the "Running the Experiment" block) that
+lines below and tell us in the TEMPLATE.md (see the "Running the Experiment" block) that
 this file should be executed first.
 """
 
